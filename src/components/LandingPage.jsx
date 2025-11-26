db44b9be60ee8edd831c32f8e751db3b26700e0b
Julian Nill <sportjuliannill@gmail.com>
2025-11-26 16:11:30 +0000

    e

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,4 +1,4 @@
-kimport React from "react";
+import React from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import FeaturesGrid from "../components/FeaturesGrid";
