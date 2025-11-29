07337a69e59be81c973dc17a6ac723f878d8b41e
Julian Nill <sportjuliannill@gmail.com>
2025-11-29 07:41:19 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./components/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
