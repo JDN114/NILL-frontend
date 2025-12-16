1090d1b64525c0b52db22e5f09a08df70b88ba40
Julian Nill <sportjuliannill@gmail.com>
2025-12-16 17:14:06 +0000

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,17 @@
+// frontend/src/services/api.js
 import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL || "https://api.nillai.de";
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true,
+  withCredentials: true, // 🍪 wichtig für Session-Cookies
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// 🔑 JWT automatisch mitsenden
+// JWT automatisch mitsenden
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
   if (token) {
@@ -16,14 +20,8 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-// ---------- AUTH ----------
-export const loginUser = async (email, password) => {
-  const res = await api.post("/auth/login", { email, password });
-  localStorage.setItem("access_token", res.data.access_token);
-  return res.data;
-};
+// -------------------- Gmail --------------------
 
-// ---------- GMAIL ----------
 export const getGmailAuthUrl = async () => {
   const res = await api.get("/gmail/auth-url");
   return res.data;
@@ -31,12 +29,6 @@ export const getGmailAuthUrl = async () => {
 
 export const getGmailStatus = async () => {
   const res = await api.get("/gmail/status");
-  return res.data;
-};
-
-// ---------- Gmail ----------
-export const getGmailEmails = async () => {
-  const res = await api.get("/gmail/emails");
   return res.data;
 };
 
