1f336df924dcff8f0a4fead355e6141e8e0e1aa2
Julian Nill <sportjuliannill@gmail.com>
2025-12-20 19:17:38 +0000

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+kimport React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+
+import LandingPage from "./components/LandingPage";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Landing Page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Auth */}
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
+
+        {/* App */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
