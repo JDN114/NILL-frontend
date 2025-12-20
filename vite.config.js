47d3826709e1a750303e9c73164004ae1201d857
Julian Nill <sportjuliannill@gmail.com>
2025-12-20 08:57:26 +0000

    d

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    sourcemap: true,        // für Debugging/Monitoring
-    target: 'esnext',       // moderne Browser
+    outDir: "dist", // Output für Vercel
+    emptyOutDir: true,
   },
-})
+  server: {
+    port: 5173,
+    open: true,
+  }
+});
