767ae22c23acd5314c2f30af21ce2dc10a00a9ca
Julian Nill <sportjuliannill@gmail.com>
2025-12-27 10:07:04 +0000

    d

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,7 +4,6 @@ import Hero from "../components/Hero";
 import FeaturesGrid from "../components/FeaturesGrid";
 import HowItWorks from "../components/HowItWorks";
 import Pricing from "../components/Pricing";
-import About from "../components/About";
 import Sustainability from "../components/Sustainability";
 import ContactForm from "../components/ContactForm";
 import CTABar from "../components/CTABar";
@@ -19,7 +18,6 @@ export default function LandingPage() {
         <FeaturesGrid />
         <HowItWorks />
         <Pricing />
-        <About />
         <Sustainability />
         <section id="contact" className="py-16">
           <div className="max-w-3xl mx-auto px-6">
