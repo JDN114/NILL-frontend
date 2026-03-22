7056217824da207f84f630a83f0ff3c4d2c34792
Julian Nill <sportjuliannill@gmail.com>
2026-03-22 09:37:11 +0000

    D

--- src/components/accounting/InvoiceList.jsx
+++ src/components/accounting/InvoiceList.jsx
@@ -1,4 +1,4 @@
-kimport { useState, useMemo } from "react";
+	import { useState, useMemo } from "react";
 import api from "../../services/api";
 
 export default function InvoiceList({ invoices = [], onUpdated }) {
