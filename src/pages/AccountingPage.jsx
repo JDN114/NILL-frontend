12b1cfdbbba2c8a7ffbc51f4ef5b4ce96542f804
Julian Nill <sportjuliannill@gmail.com>
2026-03-23 18:37:35 +0000

    D

--- src/pages/AccountingPage.jsx
+++ src/pages/AccountingPage.jsx
@@ -4,6 +4,7 @@ import Card from "../components/ui/Card";
 import InvoiceList from "../components/accounting/InvoiceList";
 import BankInsights from "../components/accounting/BankInsights";
 import TaxDashboard from "../components/accounting/TaxDashboard";
+import ReceiptUploadModal from "../components/accounting/ReceiptUpload";
 import api from "../services/api";
 
 import {
