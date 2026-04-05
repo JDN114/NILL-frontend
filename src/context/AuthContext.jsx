fd8eaef7bf9c7447492468b084e3f5c4ff45b98f
Julian Nill <sportjuliannill@gmail.com>
2026-04-05 19:57:28 +0000

    D

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import api from "../services/api";
 
 export const AuthContext = createContext(null);
