131a558e22783012d55bb4ce68196077d9933948
Julian Nill <sportjuliannill@gmail.com>
2026-04-13 19:13:43 +0000

    D

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ import VerificationFailed from "./pages/VerificationFailed";
 import InviteAcceptPage from "./pages/InviteAcceptPage";
 import FeatureRoute from "./FeatureRoute";
 import UpgradePage from "./pages/UpgradePage";
-import Delivery_notes_Page from "./pages/Workflow_delivery_notes_page.py";
+import Delivery_notes_Page from "./pages/Workflow_delivery_notes_page.jsx";
  
 function App() {
   return (
