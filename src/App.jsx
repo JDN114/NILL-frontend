28ca25244568dc32dd4605ba57e17000f64a68e9
Julian Nill <sportjuliannill@gmail.com>
2026-04-20 13:24:21 +0000

    D

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ import InviteAcceptPage from "./pages/InviteAcceptPage";
 import FeatureRoute from "./FeatureRoute";
 import UpgradePage from "./pages/UpgradePage";
 import WorkflowDeliveryNotes from "./pages/Workflow_delivery_notes_page.jsx";
-import NILLModul from "./pages/NILLModul.jsx";
+import NILLModule from "./pages/NILLModule.jsx";
  
 function App() {
   return (
@@ -146,7 +146,7 @@ function App() {
 		  <ProtectedRoute><AdminPage /></ProtectedRoute>
 		}/>
                 <Route path="/dashboard/NILL-Secretary" element={
-                  <ProtectedRoute><NILLModul /></ProtectedRoute>
+                  <ProtectedRoute><NILLModule /></ProtectedRoute>
                 }/>
 	      </Routes>
             </Router>
