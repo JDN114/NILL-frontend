ae33b166c90b05a31994d8318f2ed101499fab7f
Julian Nill <sportjuliannill@gmail.com>
2026-05-06 21:09:22 +0000

    D

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,8 @@
+import { useEffect } from "react";
+
 export default function LandingPage() {
-  return (
-    <iframe
-      src="/landing.html"
-      style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', border: 'none', zIndex: 0 }}
-      title="NILL Landing Page"
-    />
-  );
+  useEffect(() => {
+    window.location.replace("/landing.html");
+  }, []);
+  return null;
 }
